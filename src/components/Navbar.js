16a15cc4ead5f92faad99f2a56a2c485f46db830
mattishii26 <chihiro2657@gmail.com>
2020-03-04 17:17:13 -0800

    Initial Gatsby Personal Portfolio Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Navbar = ({ link }) => (
+    <div>
+        test
+    </div>
+)
+
+const NavItem = ({link}) =>{
+
+}
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,18 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import Navbar from '../components/Navbar'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <SEO title="Home"
+         description="Hello there! I am Matthew Ishii. I am a Software Engineer that enjoys approaching modern problems and changing them to modern solutions. Check out my projects and blog!"
+         canonical="https://mishiitech.com/"
+    />
+    
+    <Navbar/>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
